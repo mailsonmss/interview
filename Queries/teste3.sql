ab50f79e3399a96ca56997e8ecb0357dc6951108
Mailson Santana <mailson@mss.com>
2024-11-12 17:46:44 -0300

    Interview questions-corrigido

--- Queries/teste3.sql
+++ Queries/teste3.sql
@@ -20,10 +20,29 @@ As
             Join Salaries s
                 On  e.EmployeeID = s.EmployeeID
 )
-Select  em.EmployeeID, em.EmployeeName, em.salary as EmployeeSalary, m.salary as Manager_Salary
+Select  em.EmployeeID--, em.EmployeeName, em.salary as EmployeeSalary, m.salary as Manager_Salary
 From    c_salary_employee em
         Join c_salary_employee m
             On  em.ManagerID = m.EmployeeID
             And em.salary > m.salary -- Bigger salary?
 Where   em.EmployeeID is in (Select ManagerID from employees) -- Is it manager?
+
+
+-- Correct one:
+
+with c_salary_employee
+As
+(
+    Select  e.EmployeeID, e.EmployeeName, e.ManagerID, s.salary
+    From    employees e
+            Join Salaries s
+                On  e.EmployeeID = s.EmployeeID
+)
+Select  em.EmployeeID--, em.EmployeeName, em.salary as EmployeeSalary, m.salary as Manager_Salary
+From    c_salary_employee em
+        Join c_salary_employee m
+            On  em.ManagerID = m.EmployeeID
+            And em.salary > m.salary -- Bigger salary?
+Where   em.EmployeeID in (Select ManagerID from employees) -- Is it manager?
+
 
